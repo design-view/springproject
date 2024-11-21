867dc1e3f9f7927a8f6b0fc4acbc28c6364a85b9
greenFrontEnd <61343766+design-view@users.noreply.github.com>
2024-11-21 10:51:28 +0900

    Update kill_process.sh

--- deploy/script/kill_process.sh
+++ deploy/script/kill_process.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo docker-compose -f /home/ubuntu/deploy/script/docker-compose.yml down || true
+sudo docker-compose -f /home/ubuntu/deploy/script/docker-compose.yml down
+docker rmi pinkcandy02/springproject:latest
