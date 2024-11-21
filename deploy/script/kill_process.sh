9179bea9e337af1a936d75a211fae90952ef4910
greenFrontEnd <61343766+design-view@users.noreply.github.com>
2024-11-21 11:17:14 +0900

    Update kill_process.sh

--- deploy/script/kill_process.sh
+++ deploy/script/kill_process.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# docker-compose로 컨테이너 종료 및 제거
 sudo docker-compose -f /home/ubuntu/deploy/script/docker-compose.yml down
-docker rmi pinkcandy02/springproject:latest
+
+# 종료된 후, 이미지 삭제 (이미지 사용 중인 컨테이너가 없는지 확인)
+sudo docker rmi -f pinkcandy02/springproject:latest
